9dd4eeeeafcadfd1146e2ad3b40d286fc496724b
Coffey May <jcoffeymay6@gmail.com>
2019-12-03 14:54:34 -0600

    changed heads of state header

--- scripts/iceland/HeadsOfState/I-HOS-Component.js
+++ scripts/iceland/HeadsOfState/I-HOS-Component.js
@@ -2,7 +2,7 @@ const SingleHosComponent = (headOfState) => {
     return `
         <section class="headsOfState">
         <ul>
-        <h3>Heads of State</h3>
+       
           <img src ="${headOfState.image}" class="kingPic">
           <li> Name: ${headOfState.name}</li>
           <li> Age: ${headOfState.age}</li>
