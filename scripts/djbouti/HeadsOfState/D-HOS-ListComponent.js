1f952963ab5db89dbdb96552851eb32b85496e0a
jamesnitz <jamesanitz2@gmail.com>
2019-12-04 11:42:39 -0600

    styled heads of state

--- scripts/djbouti/HeadsOfState/D-HOS-ListComponent.js
+++ scripts/djbouti/HeadsOfState/D-HOS-ListComponent.js
@@ -3,7 +3,7 @@ import SingleHosComponent from "./D-HOS-Component.js"
 
 const importedheadsOfState = useheadsOfState()
 const headsOfState = document.querySelector(".headsOfState")
-headsOfState.innerHTML = `<h3>Heads of State</h3>`
+headsOfState.innerHTML = `<h3 class = "HOS__Header">Heads of State</h3>`
 const DHOSListComponent = () => {
     for (const singleHos of importedheadsOfState) {
         headsOfState.innerHTML += SingleHosComponent(singleHos)
